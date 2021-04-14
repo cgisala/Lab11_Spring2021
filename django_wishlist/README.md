a7ea126a00a287a971ccdfe34cacda7b075d2a80
chris <chris@debian>
2021-04-14 05:58:27 -0500

    First commit

---
+++ django_wishlist/README.md
@@ -0,0 +1,24 @@
+<h1>Lab 10 Spring 2021</h1>
+
+<h2>Setup Instructions</h2>
+
+<p>
+
+<ol>
+<li>Make sure you have python installed in your system</li>
+<li>Do this from the command line/terminal</li>
+<li>Navigate into that directory</li>
+<li>Type the command for your own OS</li>
+</ol>
+
+<h4>To create a virtual environment</h4>
+Type: python3 - venv (virutal environment name)
+
+<h4>To activate a virtual environment</h4>
+For Mac or Linux
+Type: source (virutual environment name)/bin/activate
+
+For Windows PC
+Type: (virutual environment name)\Scripts\activate
+
+</p>
---
+++ django_wishlist/wishlist/travel_wishlist/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Place
+
+class NewPlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('name', 'visited')
+
+#Create a custom date input field, otherwise would get a plain text field.
+class DateInput(forms.DateInput):
+    input_type = 'date'     #Override the default input type, which is 'text'.
+
+#Takes in note, date visited, and photo data for the Place model
+class TripReviewForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('notes', 'date_visited', 'photo')
+        widgets = {
+            'date_visited': DateInput()
+        }
